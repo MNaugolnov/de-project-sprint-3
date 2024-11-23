8cc6a98e64b49598827670a5f1e02a38af5faaf4
MNaugolnov <naugolnov@gmail.com>
2024-11-23 23:12:35 +0100

    Delete alter_tables.sql

--- alter_tables.sql
+++
@@ -1,3 +0,0 @@
--- однократные действия в dbeaver
-alter table	staging.user_order_log add column status varchar(15) not null default 'shipped';
-alter table	mart.f_sales add column status varchar(15) not null default 'shipped';
